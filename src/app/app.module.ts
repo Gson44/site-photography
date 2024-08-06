8f0a14294edd7c444229bcb4b0db168adca684bc
Gerson <arectech49@gmail.com>
2024-08-06 08:09:37 -0600

    First-Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroImageComponent } from './hero-image/hero-image.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroImageComponent,
+    GalleryComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
